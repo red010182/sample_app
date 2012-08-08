949d6a42e0c64970f854088ea493c21fce11fbb8
tpy <p.truesword@gmail.com>
2012-08-09 00:39:24 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '84dc986d97be31900ff932f635028676d2a628f61cbcb62c49157b8f71a3df5ec05ee085f5a6193113aca6a3979ecd53fc848495d04c77df8d27337448c0c8f6'
